3ca5220fccf3edaab11154e22debd7ab0842b55d
Mac Parthum <macparthum@gmail.com>
2020-09-28 17:50:35 -0400

    updated src and App.js

--- src/App.js
+++ src/App.js
@@ -12,20 +12,6 @@ import EmployeeTable from "./components/EmployeeTable";
 function App() {
   return (
     <>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
 
       <Nav />
       <Layout>
